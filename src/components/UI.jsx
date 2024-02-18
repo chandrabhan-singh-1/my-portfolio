ad572f8739a7da87c3f2a6e916cff2d25d922457
chandrabhan-singh-1 <chandrabhansingh813@gmail.com>
2024-02-18 23:40:23 +0530

    updates

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -8,13 +8,13 @@ const UI = () => {
     <div className="fixed inset-0 pointer-events-none">
       <section
         className={`flex w-full h-full flex-col items-center justify-center 
-      duration-500
+      duration-500 
       ${currentPage === "home" ? "" : "opacity-0"}`}
       >
         <div className="h-[66%]"></div>
         <button
           onClick={() => setCurrentPage("store")}
-          className="pointer-events-auto py-4 px-8 text-lg bg-gradient-to-b from-teal-600 to-sky-600 text-sky-100 tracking-wider font-black rounded-full cursor-pointer transition-colors duration-500 hover:scale-105 brightness-110"
+          className="absolute top-[30rem] pointer-events-auto py-4 px-8 text-lg bg-gradient-to-b from-teal-600 to-sky-600 text-sky-100 tracking-wider font-black rounded-full cursor-pointer transition-colors duration-500 hover:scale-105 brightness-110"
         >
           ENTER
         </button>
