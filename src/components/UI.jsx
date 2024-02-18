3e8e96270e83042abfaa54878fa6b2584e0ecf38
chandrabhan-singh-1 <chandrabhansingh813@gmail.com>
2024-02-19 01:36:28 +0530

    Button positioned

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -14,7 +14,7 @@ const UI = () => {
         <div className="h-[66%]"></div>
         <button
           onClick={() => setCurrentPage("store")}
-          className="absolute top-[30rem] pointer-events-auto py-4 px-8 text-lg bg-gradient-to-b from-teal-600 to-sky-600 text-sky-100 tracking-wider font-black rounded-full cursor-pointer transition-colors duration-500 hover:scale-105 brightness-110"
+          className="absolute top-[35rem] pointer-events-auto py-4 px-8 text-lg bg-gradient-to-b from-teal-600 to-sky-600 text-sky-100 tracking-wider font-black rounded-full cursor-pointer transition-colors duration-500 hover:scale-105 brightness-110"
         >
           ENTER
         </button>
